b24b58edbecf7849a06022caf703eed55c381e80
ishmaelali33 <ishmaelali33@gmail.com>
2020-10-28 13:22:00 -0400

    commit tree ed9c2d5f20d4f8cbbb6a6e01e2fa83f6b48a0660

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each("1", "2", "3", "4")
+    i = 0
+    my_each(collection) do |i|
+      puts i
+    while i < my each.length
+      yield(my each[i])
+      i = i + 1
+    end
 end
